43a773e83e86e9e963ca4e74e2db6a9a670a3e89
lg-png <luka.goshteliani.1@btu.edu.ge>
2026-04-13 16:08:27 +0400

    Nav bar and Slider

---
+++ script.js
@@ -0,0 +1,40 @@
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalID = null;
+
+//initalizeSlider();
+
+document.addEventListener("DOMContentLoaded", initalizeSlider);
+
+function initalizeSlider() {
+
+    if(slides.length > 0) {
+
+    slides[slideIndex].classList.add("displaySlide");
+    // setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index) {
+
+    if(index >= slides.length){
+        slideIndex = 0;
+    }else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide() {
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
